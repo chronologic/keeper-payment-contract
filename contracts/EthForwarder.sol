663561c6cbcc0c7be1e763d304eea06d42bb0b1d
Marcin <zolkiewski.marcin@gmail.com>
2021-01-24 16:25:04 +0100

    added _to address to events and improved tests

--- contracts/EthForwarder.sol
+++ contracts/EthForwarder.sol
@@ -7,20 +7,20 @@ contract EthForwarder is Ownable {
 
   address payable public destinationAddress;
 
-  event Forwarded(address indexed _sender, uint _amount);
-  event Flushed(address indexed _sender, uint _amount);
+  event Forwarded(address indexed _from, address indexed _to, uint _amount);
+  event Flushed(address indexed _from, address indexed _to, uint _amount);
 
   constructor() {
     destinationAddress = msg.sender;
   }
 
   function pay() external payable {
-    emit Forwarded(msg.sender, msg.value);
+    emit Forwarded(msg.sender, destinationAddress, msg.value);
     _send(destinationAddress, msg.value);
   }
 
   function flush() external {
-    emit Flushed(msg.sender, address(this).balance);
+    emit Flushed(msg.sender, destinationAddress, address(this).balance);
     _send(destinationAddress, address(this).balance);
   }
 
--- test/ethForwarder.spec.js
+++ test/ethForwarder.spec.js
@@ -17,7 +17,7 @@ contract("EthForwarder", (accounts) => {
     const amountToSend = web3.utils.toWei("3", "ether");
     const beficiaryBalanceBefore = await web3.eth.getBalance(beneficiaryAddress);
 
-    await instance.contract.methods.pay().send({ from: senderAddress, to: beneficiaryAddress, value: amountToSend });
+    await instance.contract.methods.pay().send({ from: senderAddress, value: amountToSend });
 
     const beficiaryBalanceAfter = await web3.eth.getBalance(beneficiaryAddress);
     const beneficiaryBalanceDiff = BigNumber.from(beficiaryBalanceAfter).sub(beficiaryBalanceBefore).toString();
@@ -36,7 +36,34 @@ contract("EthForwarder", (accounts) => {
 
     const forwardedEvent = res.events.Forwarded;
 
-    expect(forwardedEvent.returnValues._sender).to.equal(senderAddress);
+    expect(forwardedEvent.returnValues._from).to.equal(senderAddress);
+    expect(forwardedEvent.returnValues._to).to.equal(beneficiaryAddress);
     expect(forwardedEvent.returnValues._amount).to.equal(amountToSend);
+  });
+
+  it("allows owner to change destination address", async () => {
+    const ownerAddress = accounts[0];
+    const newDestinationAddress = accounts[2];
+
+    let thrown = false;
+    try {
+      await instance.contract.methods.setDestinationAddress(newDestinationAddress).send({ from: ownerAddress });
+    } catch (e) {
+      thrown = true;
+    }
+    expect(thrown).to.equal(false);
+  });
+
+  it("does not allow non-owner to change destination address", async () => {
+    const notOwnerAddress = accounts[1];
+    const newDestinationAddress = accounts[2];
+
+    let thrown = false;
+    try {
+      await instance.contract.methods.setDestinationAddress(newDestinationAddress).send({ from: notOwnerAddress });
+    } catch (e) {
+      thrown = true;
+    }
+    expect(thrown).to.equal(true);
   });
 });
